223d9dde83a35b7a18de0241377a5cd75670d870
Steven van den Hout <stevenvandenhout@gmail.com>
2014-09-24 21:24:29 +0200

    login flow

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,31 @@
+var loginReq = Titanium.Network.createHTTPClient();
+
+loginReq.onLoad = function(e) {
+  var json = this.responseText;
+  var response = JSON.parse(json);
+  if (response.logged == true) {
+    alert("Token = " + response);
+  }else {
+    alert("AWFULL");
+  }
+};
+
 function login(e) {
-    alert($.label.text);
+  if($.email.value != '' && $.password.value != '') {
+    loginReq.open("POST","http://sevenmatchestest.herokuapp.com/api/oauth/token");
+
+    var params = {
+      grant_type: "password",
+      client_id: "sevenmatches-android",
+      client_secret: "S3v3nm4tch3s8774!",
+      username: $.email.value,
+      password: $.password.value
+    };
+    
+    loginReq.send(params);
+  }else {
+    alert("Email/Wachtwoord moeten ingevuld zijn");
+  }
 }
 
 $.index.open();
