f17c32ae50bd28aa099be6fec474a38ba98cab88
Steven van den Hout <stevenvandenhout@gmail.com>
2014-10-08 08:51:57 +0200

    built player reviews

--- app/controllers/reviews.js
+++ app/controllers/reviews.js
@@ -1,1 +1,32 @@
 var args = arguments[0] || {};
+console.log(args);
+
+var reviewsReq = Titanium.Network.createHTTPClient({
+  onload: function(e) {
+    var json = this.responseText;
+    reviews = JSON.parse(json);
+    
+    var items = _.map(reviews, function(review) {
+      var avg = (
+        review.personality.avg +
+        review.technique.avg +
+        review.tactics.avg +
+        review.physical.avg
+      ) / 4;
+      
+      return {
+        "date": {text: review.parseDate},
+        "score": {text: avg}
+      };
+    });
+    
+    $.reviewList.sections[0].setItems(items);
+  },
+  onerror: function(e) {
+    console.log(this.responseText);
+  }
+});
+
+reviewsReq.open("GET", "http://localhost:3000/api//reviews/all/" + args._id);
+reviewsReq.setRequestHeader("Authorization", Alloy.Globals.authHeader);
+reviewsReq.send();
