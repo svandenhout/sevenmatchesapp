26f403986045db2fab96bdb20729563efc1fecc8
Steven van den Hout <stevenvandenhout@gmail.com>
2014-10-14 19:42:14 +0200

    added real time numbers next to sliders

--- app/controllers/createReview.js
+++ app/controllers/createReview.js
@@ -1,5 +1,8 @@
 var args = arguments[0] || {};
-
+var personality = [];
+var technique = [];
+var tactics = [];
+var physical = [];
 var review = {
   playerId: args._id,
   personality: {
@@ -22,33 +25,33 @@ var reviewFormReq = Titanium.Network.createHTTPClient({
     var reviewForm = JSON.parse(json);
     
     var personalityTitle = [{
-      title: {text: "1/4 Persoonlijkheid"}, 
-      template: "reviewListHeader", 
+      title: {text: "1/4 Persoonlijkheid"},
+      template: "reviewListHeader",
       backgroundColor: "#FFF"
     }];
     
     var techniqueTitle = [{
-      title: {text: "2/4 Techniek"}, 
-      template: "reviewListHeader", 
+      title: {text: "2/4 Techniek"},
+      template: "reviewListHeader",
       backgroundColor: "#FFF"
     }];
     
     var tacticsTitle = [{
       title: {text: "3/4 Taktiek"}, 
-      template: "reviewListHeader", 
+      template: "reviewListHeader",
       backgroundColor: "#FFF"
     }];
     
     var physicalTitle = [{
-      title: {text: "4/4 Fysiek"}, 
-      template: "reviewListHeader", 
+      title: {text: "4/4 Fysiek"},
+      template: "reviewListHeader",
       backgroundColor: "#FFF"
     }];
     
-    var personality = mapReviewForm(reviewForm.personality, "personality");
-    var technique = mapReviewForm(reviewForm.technique, "technique");
-    var tactics = mapReviewForm(reviewForm.tactics, "tactics");
-    var physical = mapReviewForm(reviewForm.physical, "physical");
+    personality = mapReviewForm(reviewForm.personality, "personality");
+    technique = mapReviewForm(reviewForm.technique, "technique");
+    tactics = mapReviewForm(reviewForm.tactics, "tactics");
+    physical = mapReviewForm(reviewForm.physical, "physical");
     
     $.reviewItemList.sections[0].appendItems(personalityTitle);
     $.reviewItemList.sections[1].appendItems(personality);
@@ -61,6 +64,8 @@ var reviewFormReq = Titanium.Network.createHTTPClient({
     
     $.reviewItemList.sections[6].appendItems(physicalTitle);
     $.reviewItemList.sections[7].appendItems(physical);
+    
+    console.log($.Winnersinstelling);
   },
   onerror: function(e) {
     console.log(this.responseText);
@@ -74,38 +79,50 @@ function mapReviewForm(reviewFormSection, sectionName) {
   for(var i = 0; i < reviewFormSection.length; i++) {
     review[sectionName].scores.push(0);
     section.push({
-      itemTitle: {
-        text: reviewFormSection[i]
-      }
+      itemTitle: {text: reviewFormSection[i]},
+      score: {text: 0}
     });
     if(i % 2 === 0) section[i].template = "reviewItemOdd";
   };
-
+  
   return section;
 }
 
 // this needs to change the number after the score
 function changeScore(e) {
-  console.log(this);
   if(e.sectionIndex === 1) {
+    var item = $.reviewItemList.sections[e.sectionIndex].items[e.itemIndex];
+    item.score.text = Math.round(this.getValue() * 100) / 100;
+    $.reviewItemList.sections[e.sectionIndex].updateItemAt(e.itemIndex, item);
     review.personality.scores[e.itemIndex] = 
         Math.round(this.getValue());
   }
   
   if(e.sectionIndex === 3) {
+    var item = $.reviewItemList.sections[e.sectionIndex].items[e.itemIndex];
+    item.score.text = Math.round(this.getValue() * 100) / 100;
+    $.reviewItemList.sections[e.sectionIndex].updateItemAt(e.itemIndex, item);
     review.technique.scores[e.itemIndex] = 
         Math.round(this.getValue());
   }
   
   if(e.sectionIndex === 5) {
+    var item = $.reviewItemList.sections[e.sectionIndex].items[e.itemIndex];
+    item.score.text = Math.round(this.getValue() * 100) / 100;
+    $.reviewItemList.sections[e.sectionIndex].updateItemAt(e.itemIndex, item);
     review.tactics.scores[e.itemIndex] = 
         Math.round(this.getValue());
   }
   
   if(e.sectionIndex === 7) {
+    var item = $.reviewItemList.sections[e.sectionIndex].items[e.itemIndex];
+    item.score.text = Math.round(this.getValue() * 100) / 100;
+    $.reviewItemList.sections[e.sectionIndex].updateItemAt(e.itemIndex, item);
     review.physical.scores[e.itemIndex] = 
         Math.round(this.getValue());
   }
+  
+  
 }
 
 function createReview(e) {
